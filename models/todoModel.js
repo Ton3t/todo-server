2b6cf9aeef3f7113c81c289e17e8e3acd0bdd501
Ton3t <elton3t@gmail.com>
2024-04-18 21:19:19 +0200

    estado del to-do

--- models/todoModel.js
+++ models/todoModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const todoSchema = new mongoose.Schema(
   {
     description: { type: String },
+    active: {type: Boolean, default: true},
   },
   {
     timestamps: true,
